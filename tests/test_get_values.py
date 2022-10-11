17f937549c3ebd423dcde53ce3015579dad28e68
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2022-10-11 17:08:50 -0300

    First tests

---
+++ tests/test_get_values.py
@@ -0,0 +1,19 @@
+from src.calculator import get_values
+from faker import Faker
+
+fake = Faker(locale='en_US')
+
+
+def test_get_values(monkeypatch):
+
+    # Given
+    first_entry = fake.random_number()
+    second_entry = fake.random_number()
+
+    # When
+    inputs = iter([first_entry, second_entry])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    result = get_values()
+
+    # Then
+    assert result == (first_entry, second_entry)
