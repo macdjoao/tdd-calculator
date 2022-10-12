a119aa9a3c082d0cbbab7a032e77cd40b3e4dec5
João Macedo <macdjoao@gmail.com>
2022-10-12 13:06:39 -0300

    Adding a docstring for fixture numbers

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,11 @@ fake = Faker(locale='en_US')
 
 @pytest.fixture(scope="module")
 def numbers():
+    """numbers
+
+    Returns:
+        tuple: returns a tuple with two random numbers for use in test functions
+    """
     first_entry = fake.random_number()
     second_entry = fake.random_number()
     payload = (first_entry, second_entry)
